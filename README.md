6a59e0c048977b65412505d488669a8a235e6269
Juan Lopes <me@juanlopes.net>
2011-09-30 19:09:58 -0300

    cutting sticks

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 **Dynamic Programming**
 
 * 10891 - [Game of Sum](http://uva.onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=1832)
+* 10003 - [Cutting Sticks](http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=944)
--- dynamic-programming/10003.cpp
+++ dynamic-programming/10003.cpp
@@ -1,4 +1,4 @@
-#define MAX 101
+#define MAX 1001
 #include <iostream>
 #include <cstring>
 #include <climits>
@@ -8,31 +8,31 @@ int T[MAX][MAX], S[MAX], n;
 bool V[MAX][MAX];
 
 int TT(int a, int b) {
-    if (b<a) return 0;
+    if (a+1==b) return 0;
     if (V[a][b]) return T[a][b];
 
-    int maxx = INT_MIN;
-    for(int i=a; i<=b; i++)
-        maxx = max(maxx, S[b]-S[a-1] - TT(i+1,b));
-    
-    for(int i=b; i>=a; i--)
-        maxx = max(maxx, S[b]-S[a-1] - TT(a,i-1));
+    int minn = INT_MAX;
+    for(int i=a+1; i<b; i++)
+        minn = min(minn, TT(a,i) + TT(i,b) + S[b]-S[a]);
     
     V[a][b] = true;
-    return T[a][b] = maxx;
+    return T[a][b] = minn;
 }
 
 int main() {
-    while(cin >> n, n) {
+    int t;
+    while(cin >> t, t) {
+        cin >> n;
+        
         memset(S, 0, sizeof(S));
         memset(V, 0, sizeof(V));
         S[0] = 0;
         for(int i=1; i<=n; i++) {
             cin >> S[i];
-            S[i] += S[i-1];
         }
+        S[n+1] = t;
            
-        cout << 2*TT(1, n)-S[n]-S[0] << endl; 
+        cout << "The minimum cutting is " << TT(0, n+1) << "." << endl; 
         
     }    
 }
