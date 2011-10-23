3906d9c8fc728bc26d8c1d825080698d956b3c56
Juan Lopes <me@juanlopes.net>
2011-10-23 01:58:43 -0200

    damn tabs

--- dynamic-programming/11703.cpp
+++ dynamic-programming/11703.cpp
@@ -7,20 +7,20 @@ using namespace std;
 int K[1000001];
 
 int main() {
-	K[0] = 1;
-	for(int i=1; i<100; i++) {
-		int a = (int)(i-sqrt(i));
-		int b = (int)log(i);
-		int c = (int)(i*pow(sin(i), 2));
-		assert(a<i); assert(b<i); assert(c<i); 
-		K[i] = (K[a] + K[b] + K[c])%1000000;
-	}
+    K[0] = 1;
+    for(int i=1; i<100; i++) {
+        int a = (int)(i-sqrt(i));
+        int b = (int)log(i);
+        int c = (int)(i*pow(sin(i), 2));
+        assert(a<i); assert(b<i); assert(c<i); 
+        K[i] = (K[a] + K[b] + K[c])%1000000;
+    }
 
-	int n;
+    int n;
 
-	while(cin >> n, n>-1)
-		cout << K[n] << endl;
-	
+    while(cin >> n, n>-1)
+        cout << K[n] << endl;
+    
 
-	return 0;
+    return 0;
 }
