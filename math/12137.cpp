8e5cefc546897c84c667b7f8eec524124c614358
Juan Lopes <me@juanlopes.net>
2011-10-28 12:16:52 -0200

    12137

--- math/12137.cpp
+++ math/12137.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
-#include <vector>
-#include <cstring>
+#include <string.h>
+#include <stdio.h>
 #define ull unsigned long long
-#define MAX 1000001
-using namespace std;
 
-bool P[MAX];
-vector<int> W;
+int W[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 }, wn = 23;
 
-ull pow(ull a, int b) {
+inline ull div(const ull& a, const ull& b, ull &r) {
+    r = a/b;
+    return a-r*b;
+}
+
+inline ull pow(const ull& a, const int b) {
     if (b==0) return 1;
     ull tmp = b&1 ? a : 1;
     ull r = pow(a, b>>1);
@@ -16,38 +17,29 @@ ull pow(ull a, int b) {
 }
 
 int main() {
-    memset(P, true, sizeof(P));
-    P[0] = P[1] = false;
-    for(ull i=2; i<MAX; i++) {
-        if (P[i]) {
-            W.push_back(i);
-            for(ull j=i*i; j<MAX; j+=i)
-                P[j] = false;
-        }
-    }
-    
-    ull n, t=0;
-    while(cin >> n, n) {
+    ull n;
+    int t=0;
+    while(scanf("%llu", &n), n) {
         ull ncopy = n;
         ull step = 1;
-        for(int i=0; ncopy>1 && i<W.size(); i++) {
+        for(int i=0; ncopy>1 && i<wn; i++) {
             int power=0;
-            while(ncopy%W[i]==0) {
-                ncopy/=W[i];
+            ull divr;
+            while(div(ncopy, W[i], divr)==0) {
+                ncopy = divr;
                 power++;
             }
             step *= pow(W[i], (power+1)/2);
         }
         step *= ncopy;
         
-        ull result = n/step;
-        if (n%step==0) result--;
+        ull result;
+        if (div(n, step, result)==0) result--;
         result *= 8;
         
-        cout << "Case " << ++t << ": ";
         if(result)
-            cout << result << endl;
+            printf("Case %d: %llu\n",++t, result);
         else
-            cout << "Impossible" << endl;
+            printf("Case %d: Impossible\n", ++t);
     }
 }
