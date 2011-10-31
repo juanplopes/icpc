3311b18c67347b8782c0c777ff45dcc2f9c297ef
Juan Lopes <me@juanlopes.net>
2011-10-31 17:03:53 -0200

    merge

--- merge.rb
+++ merge.rb
@@ -6,14 +6,12 @@ def markdown(text)
     Redcarpet.new(text, *options).to_html
 end
 
-puts "<style>div.break { page-break-before: always; }</style>"
+puts "<style>.break { page-break-after: always; }</style>"
 
 Dir.glob("**/").each do |dir|
-    puts markdown(File.read(File.join(dir, 'README.md'))) 
-    puts "<div class='break'>&nbsp;</div>"
+    puts "<div class='break'>" + markdown(File.read(File.join(dir, 'README.md'))) + "</div>" 
     Dir.glob(File.join(dir, "*.cpp")).sort_by {|s| File.split(s)[1].split('.')[0].to_i }.each do |file|
-        puts markdown("#"+file+"\n``` cpp\n" + File.read(file) + "\n ```\n")
-        puts "<div class='break'>&nbsp;</div>"
+        puts "<div class='break'>" + markdown("#"+file+"\n``` cpp\n" + File.read(file) + "\n ```\n") + "</div>"
     end
 end
 
