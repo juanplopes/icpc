d17c4dd0d2c0573c5b456b49017b9812b8188573
Juan Lopes <me@juanlopes.net>
2011-10-31 23:48:43 -0200

    merge improved

--- merge.rb
+++ merge.rb
@@ -1,17 +1,41 @@
 require 'rubygems'
 require 'redcarpet'
+require 'CGI'
 
 def markdown(text)
     options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
     Redcarpet.new(text, *options).to_html
 end
 
-puts "<style>.break { page-break-after: always; page-break-inside: avoid; }</style>"
+puts '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+    <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Juan Lopes</title>
+        <script type="text/javascript" src="_tools/shCore.js"></script>
+        <script type="text/javascript" src="_tools/shBrushCpp.js"></script>
+        <link type="text/css" rel="stylesheet" href="_tools/shCoreDefault.css"/>
+        <script type="text/javascript">SyntaxHighlighter.all();</script>
+    </head>
+    <body>'
+
+puts "<style>.break { page-break-after: always;; }</style>"
 
 Dir.glob("**/").each do |dir|
-    puts "<div class='break'>" + markdown(File.read(File.join(dir, 'README.md'))) + "</div>" 
-    Dir.glob(File.join(dir, "*.cpp")).sort_by {|s| File.split(s)[1].split('.')[0].to_i }.each do |file|
-        puts "<div class='break'>" + markdown("#"+file+"\n``` cpp\n" + File.read(file) + "\n ```\n") + "</div>"
+    if File.exists?(File.join(dir, 'README.md'))
+        puts "<div class='break'>"
+        puts markdown(File.read(File.join(dir, 'README.md')))
+        puts "</div>" 
+        puts "<div class='break'>"
+        Dir.glob(File.join(dir, "*.cpp")).sort_by {|s| File.split(s)[1].split('.')[0].to_i }.each do |file|
+            puts "<h1>#{file}</h1>"
+            puts '<pre class="brush: cpp">'
+            puts CGI::escapeHTML(File.read(file))
+            puts '</pre>'
+        end
+        puts "</div>" 
     end
 end
+
+puts '</body></html>'
 
