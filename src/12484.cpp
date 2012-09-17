7d88437f59b39ac6c298fa3465768c0bbe656fb0
Juan Lopes <me@juanlopes.net>
2012-09-17 19:39:54 -0300

    fix tabs

--- src/12484.cpp
+++ src/12484.cpp
@@ -11,23 +11,23 @@ ull T[10006], Q[10006], M[10006];
 
 
 int main() {
-	int n;
-	while(cin >> n) {
-		for(int i=1; i<=n; i++) {
-			cin >> M[i];
-			M[i]+=M[i-1];
-		}
+    int n;
+    while(cin >> n) {
+        for(int i=1; i<=n; i++) {
+            cin >> M[i];
+            M[i]+=M[i-1];
+        }
 
         memset(T, 0, sizeof(ull)*n);
 
-		for(int i=1; i<=n; i++) {
+        for(int i=1; i<=n; i++) {
 		    for(int j=0; j<=n-i; j++)
                 Q[j] = M[j+i]-M[j] - min(T[j], T[j+1]);
 
-		    swap(T, Q);
+            swap(T, Q);
 		}
 		
-		cout << T[0] << endl;
-	}
-	
+        cout << T[0] << endl;
+    }
+
 }
