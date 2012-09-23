8a056db927ae2fede620e8cfeb2488d326b21e0e
Juan Lopes <me@juanlopes.neT>
2012-09-23 15:13:52 -0300

    u1017

--- timus/1017.cpp
+++ timus/1017.cpp
@@ -10,18 +10,15 @@ using namespace std;
 ull T[MAX][MAX];
 
 int main() {
-    int n;
-    while(cin >> n) {
-        memset(T, 0, sizeof(T));
-    
-        for(int i=1;i<=n; i++) {
-            for(int j=1; j<=i; j++) {
-                T[i][j] = 1;
-                for(int k=j; k<=i; k++)
-                    T[i][j] += T[i-k][k+1];
-            }
+    for(int i=1;i<MAX; i++) {
+        for(int j=1; j<=i; j++) {
+            T[i][j] = 1;
+            for(int k=j; k<=i; k++)
+                T[i][j] += T[i-k][k+1];
         }
-        
-        cout << T[n][1]-1 << endl;
     }
+
+    int n;
+    while(cin >> n)
+        cout << T[n][1]-1 << endl;
 }
