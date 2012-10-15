d70a650ba3897c14c54d8f42399bf6ce86e0332f
Juan Lopes <me@juanlopes.neT>
2012-10-15 00:41:12 -0300

    986

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # **Dynamic Programming**
 * **Ad hoc**
     * [UVA 825 - Walking on the Safe Side](http://uva.onlinejudge.org/external/8/825.html)
+    * [UVA 986 - How Many](http://uva.onlinejudge.org/external/9/986.html)
     * [TIMUS 1017 - Staircases](http://acm.timus.ru/problem.aspx?num=1017)
     * [TIMUS 1018 - Binary Apple Tree](http://acm.timus.ru/problem.aspx?num=1018)
     * [UVA 1231 - ACORN](http://uva.onlinejudge.org/external/12/1231.html)
---
+++ uva/986.cpp
@@ -0,0 +1,40 @@
+//986
+//How Many
+//Dynamic Programming;Ad hoc
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int T[50][50][50][2];
+    
+int main() {
+    int n, r, h;
+    
+    while(cin >> n >> r >> h) {
+        memset(T, 0, sizeof(T));
+        T[0][0][0][0] = 1;
+
+        for(int i=1; i<=2*n; i++) {
+            for(int j=0; j<=2*n; j++) {
+                for(int k=0; k<=2*n; k++) {
+                    if (j<2*n) {
+                        T[i][j][k][0] += T[i-1][j+1][k][0]; 
+                        
+                        if (j+1==h && k>0)
+                            T[i][j][k][0] += T[i-1][j+1][k-1][1];
+                        else if (j+1!=h)
+                            T[i][j][k][0] += T[i-1][j+1][k][1];
+                    }
+
+                    if (j>0) {
+                        T[i][j][k][1] += T[i-1][j-1][k][0] + T[i-1][j-1][k][1];
+                    }
+  
+                }
+            }
+        }
+        
+        cout << T[n*2][0][r][0] << endl;
+
+    }
+}
