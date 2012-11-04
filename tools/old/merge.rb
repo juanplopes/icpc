27f420dd2f1ea3c9d82930196032940f900dbfee
Juan Lopes <me@juanlopes.neT>
2012-11-04 00:04:04 -0200

    adding compile command to easy html generation

--- tools/old/merge.rb
+++ tools/old/merge.rb
@@ -11,11 +11,25 @@ puts '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.or
     <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>compiled</title>
-        <script type="text/javascript" src="old/shCore.js"></script>
-        <script type="text/javascript" src="old/shBrushCpp.js"></script>
-        <link type="text/css" rel="stylesheet" href="old/shCoreDefault.css"/>
-        <script type="text/javascript">SyntaxHighlighter.all();</script>
+        <title>compiled</title>'
+puts '<script type="text/javascript">'
+puts File.read(File.join(File.dirname(__FILE__), 'shCore.js'))
+puts '</script>'
+
+puts '<script type="text/javascript">'
+puts File.read(File.join(File.dirname(__FILE__), 'shBrushCpp.js'))
+puts '</script>'
+
+puts '<style type="text/css">'
+puts File.read(File.join(File.dirname(__FILE__), 'shCoreDefault.css'))
+puts '</style>'
+
+puts '<style type="text/css">'
+puts File.read(File.join(File.dirname(__FILE__), 'compiled.css'))
+puts '</style>'
+
+
+puts '<script type="text/javascript">SyntaxHighlighter.all();</script>
     </head>
     <body>'
 
@@ -25,12 +39,14 @@ puts "<div class='break'>"
 puts markdown(File.read('README.md'))
 puts "</div>" 
 
-puts "<div class='break'>"
-Dir.glob(File.join('src', "*.cpp")).sort_by {|s| File.split(s)[1].split('.')[0].to_i }.each do |file|
-    puts "<h1>#{file}</h1>"
-    puts '<pre class="brush: cpp">'
-    puts CGI::escapeHTML(File.read(file))
-    puts '</pre>'
+puts "<div>"
+['uva', 'timus', 'spojbr'].each do |dir| 
+    Dir.glob(File.join(dir, "*.cpp")).sort_by {|s| File.split(s)[1].split('.')[0].to_i }.each do |file|
+        puts "<h1>#{file}</h1>"
+        puts '<pre class="brush: cpp">'
+        puts CGI::escapeHTML(File.read(file))
+        puts '</pre>'
+    end
 end
 puts "</div>" 
 
